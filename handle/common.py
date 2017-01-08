912f7a523fe095191d6ff944fc5360ae1f2ef926
benamazing <lxb_sysu@hotmail.com>
2017-01-08 23:20:42 +0800

    Added Bootstrap

--- handle/common.py
+++ handle/common.py
@@ -5,7 +5,7 @@ import tornado.web
 
 class Index(tornado.web.RequestHandler):
     def get(self):
-        self.redirect("http://benamazing.cn")
+        self.render("index.html")
 
     def post(self):
         req = tornado.escape.json_decode(self.request.body)
--- start.py
+++ start.py
@@ -9,7 +9,8 @@ define("port", default=8081, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
     def __init__(self):
-        tornado.web.Application.__init__(self, handlers, **settings)
+        tornado.web.Application.__init__(self, handlers,template_path=os.path.join(os.path.dirname(__file__), "templates"),static_path=os.path.join(os.path.dirname(__file__), "static"))
+
 
 
 if __name__ == '__main__':
@@ -17,5 +18,6 @@ if __name__ == '__main__':
 	tornado.options.parse_command_line()
 	http_server = tornado.httpserver.HTTPServer(app)
 	http_server.listen(options.port)
+	print 'Start listening on port 8081'
 	tornado.ioloop.IOLoop.instance().start()
     
