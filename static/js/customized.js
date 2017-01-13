5c91ebc672dcf3d5de6c39cbf6fea22c42fb28aa
Ben Li <ben.x.b.li@cargosmart.com>
2017-01-13 10:05:02 +0800

    refine customized script

--- static/js/customized.js
+++ static/js/customized.js
@@ -1,29 +1,8 @@
 $(document).ready(function(){
-	$('#hs300').DataTable({
-		buttons: [
-					{
-						extend: 'csv',
-						text: 'Export to csv'
-					},
-					{
-						extend: 'excel',
-						text: 'Export to Excel'
-					}
-		],
-		"lengthMenu": [[10,50,100, -1], [10,50,100,"All"]],
-		"ajax": "/hs300list",
-                "initComplete": function(settings, json){
-                    var t = $('#hs300').DataTable();
-                    t.buttons(0, null).containers().appendTo('body');
-                },
-		"columns": [
-					{"data": "code"},
-					{"data": "name"},
-					{"data": "weight"}
-		]
-	});
 
 	$.getJSON("/hs300list", function(result){
+
+		//Render Bar chart
 		var myChart = echarts.init(document.getElementById('chartArea'));
 		var option = {
 			title: {
@@ -48,7 +27,32 @@ $(document).ready(function(){
 			option.xAxis.data[i] = result.data[i].name;
 			option.series[0].data[i] = result.data[i].weight;
 		}
-		myChart.setOption(option)
-	});
+		myChart.setOption(option);
 
+		//Render dataTable
+		$('#hs300').DataTable({
+			buttons: [
+					{
+						extend: 'csv',
+						text: 'Export to csv'
+					},
+					{
+						extend: 'excel',
+						text: 'Export to Excel'
+					}
+			],
+			"lengthMenu": [[10,50,100, -1], [10,50,100,"All"]],
+			//"ajax": "/hs300list",
+			"data": result.data,
+            "initComplete": function(settings, json){
+                    var t = $('#hs300').DataTable();
+                    t.buttons(0, null).containers().appendTo('body');
+             },
+			"columns": [
+					{"data": "code"},
+					{"data": "name"},
+					{"data": "weight"}
+			]
+		});
+	});
 });
