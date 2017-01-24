25aa8e9b9a5c0ec0b21519b33d508ae6737e2a1b
Ben Li <ben.x.b.li@cargosmart.com>
2017-01-24 17:23:34 +0800

    add wechat menu

---
+++ wechat_manage/menu_create.py
@@ -0,0 +1,14 @@
+from config import wechat
+import json
+
+def createMenu():
+    with open("../conf/wechat_menu.json") as f:
+        menu_str = f.read()
+        menu = json.loads(menu_str)
+
+    try:
+        wechat.delete_menu()
+        result = wechat.create_menu(menu)
+        print result
+    except Exception,e:
+        print e
