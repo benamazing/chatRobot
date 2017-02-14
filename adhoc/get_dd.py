067ae04c30687d1237a12b76251afd90e650f5af
Ben Li <ben.x.b.li@cargosmart.com>
2017-02-14 17:22:19 +0800

    get_dd

---
+++ adhoc/get_dd.py
@@ -0,0 +1,84 @@
+# -*- coding:utf-8 -*-
+# encoding=utf-8
+
+__author__ = 'LIBE5'
+'''获取股票大单交易数据，股票交易超过500手的大单数'''
+
+import datetime
+import pymongo
+import threading
+import json
+import tushare as ts
+import logging
+
+THREAD_NUMS = 20
+logging.basicConfig(level=logging.WARN,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S')
+
+with open("../conf.json") as f:
+    conf_str = f.read()
+    conf = json.loads(conf_str)
+    if r'mongo_host' in conf:
+        mongo_host = conf[r'mongo_host']
+
+mongo_client = pymongo.MongoClient(host=mongo_host, port=27017)
+mongo_db = mongo_client['stock']
+hist_data_collection = mongo_db['stock_hist_data']
+
+def get_all_dd(start, end=None):
+    start_date = datetime.datetime.strptime(start, '%Y-%m-%d')
+    if end is None:
+        end_date = datetime.datetime.now() - datetime.timedelta(days=1)
+    else:
+        end_date = datetime.datetime.strptime(end, '%Y-%m-%d')
+    df = ts.get_stock_basics()
+    idx = start_date
+    while idx <= end_date:
+        date = idx.strftime('%Y-%m-%d')
+        get_dd_by_date_multi_threads(date=date, stock_list=df)
+        idx = idx + datetime.timedelta(days=1)
+
+
+def get_dd_by_date_multi_threads(date, stock_list, **kwargs):
+    count = hist_data_collection.count({"date": date})
+    if count != 0:
+        logging.warn("%s: start..." % date)
+        threads = []
+        for i in range(THREAD_NUMS):
+            t = threading.Thread(target=get_dd_by_date_single_thread, args=(date, stock_list, i,))
+            threads.append(t)
+        for t in threads:
+            t.setDaemon(True)
+            t.start()
+        for t in threads:
+            t.join()
+        logging.warn("%s: end..." % date)
+
+
+def get_dd_by_date_single_thread(date, stock_list, threadNo):
+    ind = 0
+    for code in stock_list.index:
+        ind += 1
+        if ind % THREAD_NUMS != threadNo:
+            continue
+        try:
+            df = ts.get_sina_dd(code=code, date=date, vol=500)
+            if df is None or len(df) == 0:
+                hist_data_collection.update_one({"code":code, "date":date}, {"$set":{"over_500_total":0, "over_500_buy":0, "over_500_sell": 0}})
+            else:
+                over_500_buy = 0
+                over_500_sell = 0
+                over_500_total = len(df)
+                for idx in df.index:
+                    if df.ix[idx]['type'] == '买盘':
+                        over_500_buy += 1
+                    if df.ix[idx]['type'] == '卖盘':
+                        over_500_sell +=1
+                hist_data_collection.update_one({"code":code, "date":date}, {"$set":{"over_500_total":over_500_total, "over_500_buy":over_500_buy, "over_500_sell": over_500_sell}})
+        except:
+            logging.exception("%s, %s: got exception" % (code, date))
+
+if __name__ == '__main__':
+    get_all_dd(start='2017-01-03')
+
