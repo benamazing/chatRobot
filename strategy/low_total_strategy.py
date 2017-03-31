2fe36c82eafaf3ae24e07abbcc1fab6857eaaf45
LIBE5 <ben.x.b.li@cargosmart.com>
2017-03-31 10:39:20 +0800

    low total strategy

--- strategy/low_total_strategy.py
+++ strategy/low_total_strategy.py
@@ -4,17 +4,20 @@
 import sys
 sys.path.append('..')
 
+import json
+import pymongo
 import tushare as ts
 import datetime
 from base import BaseScheduleStrategy
 
-'''低市净率*市盈率策略'''
+'''低总市值策略'''
 
 
-class LowPBPEStrategy(BaseScheduleStrategy):
+class LowTotalStrategy(BaseScheduleStrategy):
+
     def __init__(self, start, period, stock_amount, init_cap, send_mail=1, source='mongo'):
-        super(LowPBPEStrategy, self).__init__(start, period, stock_amount, init_cap, send_mail, source)
-        self.strategy_name = 'Low PB'
+        super(LowTotalStrategy, self).__init__(start, period, stock_amount, init_cap, send_mail, source)
+        self.strategy_name = 'Low Circulation Market Value'
 
         # 中证500为股票池
         # zz500 = ts.get_zz500s()
@@ -45,8 +48,8 @@ class LowPBPEStrategy(BaseScheduleStrategy):
             else:
                 df = self.get_last_trade_stock_basics_from_tushare(date)
 
-            # 限制流通市值在100-200亿
-            sorted_list = self.sort_stock_pool_by_pbpe_filter_big(df, [0, 200], date)
+            # 限制市净率在0-5之间
+            sorted_list = self.sort_stock_pool_by_total_filter_pb(df, [0, 5], date)
             target_list = sorted_list[0:self.stock_amount]
 
             codes = [item['code'] for item in target_list]
@@ -67,11 +70,10 @@ class LowPBPEStrategy(BaseScheduleStrategy):
                 if code not in self.hold_stocks.keys():
                     will_buy_list.append(target_list[codes.index(code)])
             self.buy(will_buy_list, date)
-
         self.counter += 1
 
-    # 限制流通市值 < limit
-    def sort_stock_pool_by_pbpe_filter_big(self, df, limit_range, date):
+    # 限制市净率 的 limit
+    def sort_stock_pool_by_total_filter_pb(self, df, limit_range, date):
         items = []
         for code in self.stocks_pool:
             delta = 1
@@ -86,28 +88,26 @@ class LowPBPEStrategy(BaseScheduleStrategy):
             item = dict()
             item['price'] = result[0]['close']
 
-            # 有些历史数据的outstanding是以万为单位的，所以要除以10000
-            if df.outstanding.max() > 1000000:
-                item['outstanding'] = df.ix[code]['outstanding'] / 10000
+            # 有些历史数据的total是以万为单位的，所以要除以10000
+            if df.total.max() > 1000000:
+                item['total'] = df.ix[code]['total'] / 10000
             else:
-                item['outstanding'] = df.ix[code]['outstanding']
-            item['outstanding_cap'] = item['price'] * item['outstanding']
+                item['total'] = df.ix[code]['total']
+            item['total_cap'] = item['price'] * item['total']
             item['code'] = code
             item['name'] = df.ix[code]['name']
             item['pb'] = df.ix[code]['pb']
-            item['pe'] = df.ix[code]['pe']
             if item['pb'] <= 0:
                 continue
-            if item['pe'] <= 0:
-                continue
-            if item['outstanding_cap'] < limit_range[0] or item['outstanding_cap'] > limit_range[1]:
+            if item['pb'] <= limit_range[0] or item['pb'] >= limit_range[1]:
                 continue
             # 去掉ST股
             if df.ix[code]['name'].find('*ST') >= 0:
                 continue
             items.append(item)
-        items = sorted(items, key=lambda x: x['pb'] * x['pe'])
+        items = sorted(items, key=lambda x: x['total_cap'])
         return items
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
@@ -125,6 +125,6 @@ if __name__ == '__main__':
     if len(sys.argv) == 6:
         send_mail = int(sys.argv[4])
         source = sys.argv[5]
-    s = LowPBPEStrategy(start='2016-08-30', period=int(sys.argv[1]), stock_amount=int(sys.argv[2]),
-                      init_cap=int(sys.argv[3]), send_mail=send_mail, source=source)
+    s = LowTotalStrategy(start='2016-08-30', period=int(sys.argv[1]), stock_amount=int(sys.argv[2]),
+                               init_cap=int(sys.argv[3]), send_mail=send_mail, source=source)
     s.simulate()
